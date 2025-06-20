8dcbc3c7bb98f163c3720c5c6ad40e5591cda468
HoangPoochie <a1905803@adelaide.edu.au>
2025-06-20 04:32:07 +0000

    Nothing changed

--- starthere/app.js
+++ starthere/app.js
@@ -10,6 +10,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 let db;
 
